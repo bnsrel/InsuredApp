b6dca8d41067f18832f35d01167bb52100386705
Ariel <rel.bns@gmail.com>
2019-06-07 11:03:25 +0300

    baseUrl changed

--- src/app/services/my-server.service.ts
+++ src/app/services/my-server.service.ts
@@ -5,7 +5,8 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class MyServerService {
-  baseUrl = 'http://localhost:8080/api/users/';
+  //baseUrl = 'http://localhost:8080/api/users/';
+  baseUrl = 'http://hackeru.tk/api/users/';
   constructor(private http: HttpClient) {}
 
   addUser(data) {
